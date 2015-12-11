9eea18ac4d4fa1a8b5af434bcc6d4d8bf8ce46b1
Antoine J.-P. Tixier <Tixierae@users.noreply.github.com>
2015-12-11 13:36:25 -0700

    Update weight.community.R

--- weight.community.R
+++ weight.community.R
@@ -1,8 +1,5 @@
 weight.community <-
 function(row,membership,weigth.within,weight.between){
-# author: Antoine Tixier
-# last updated: 3.17.2015
-
 if(as.numeric(membership[which(names(membership)==row[1])])==as.numeric(membership[which(names(membership)==row[2])])){
 weight=weigth.within
 }else{
